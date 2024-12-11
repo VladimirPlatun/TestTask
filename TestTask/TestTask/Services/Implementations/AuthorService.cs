5f7cf089f60b9d93e7a197f51250b8e0d5d313ce
Vladimir <platun.vova2016@gmail.com>
2024-12-11 14:28:59 +0300

    1.0

---
+++ TestTask/TestTask/Services/Implementations/AuthorService.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class AuthorService : IAuthorService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AuthorService(ApplicationDbContext context) 
+        {
+            _context = context;
+        }
+
+        public async Task<Author> GetAuthor()
+        {
+            return await _context.Books
+                .OrderByDescending(b => b.Title.Length)
+                .ThenBy(b => b.Author.Id)
+                .Select(b => b.Author)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Author>> GetAuthors()
+        {
+            return await _context.Authors
+                .Where(a => a.Books.Count(b => b.PublishDate.Year > 2025) % 2 == 0)
+                .ToListAsync();
+        } 
+    }
+}
---
+++ TestTask/TestTask/Services/Implementations/BookService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestTask.Data;
+using TestTask.Models;
+using TestTask.Services.Interfaces;
+
+namespace TestTask.Services.Implementations
+{
+    public class BookService : IBookService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Book> GetBook()
+        {
+            return await _context.Books
+                .OrderByDescending(b => b.Price * b.QuantityPublished)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Book>> GetBooks()
+        {
+            DateTime carolusRexReleaseDate = new DateTime(2012, 5, 25);
+
+            return await _context.Books
+                .Where(b => b.Title.Contains("Red", StringComparison.OrdinalIgnoreCase) && b.PublishDate > carolusRexReleaseDate)
+                .ToListAsync();
+        }
+
+    }
+}
